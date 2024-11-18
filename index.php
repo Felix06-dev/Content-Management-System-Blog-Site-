6fa5c71d00eb3fa214d55abda2ce0cd22ccabb2d
Felix06-dev <felixtaylorbeken@outlook.com>
2024-11-18 12:01:58 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+
+include('includes/config.inc.php');
+include('includes/database.inc.php');
+include('includes/functions.inc.php');
+
+include('includes/header.inc.php');
+?>
+
+this is content if index.php file
+
+<?php
+include('includes/footer.inc.php');
+?>
